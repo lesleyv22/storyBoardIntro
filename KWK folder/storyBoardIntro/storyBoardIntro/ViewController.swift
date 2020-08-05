3fc2ff03f99e5460fb3bed96897336bc287df49b
lesleyv22 <69254499+lesleyv22@users.noreply.github.com>
2020-08-05 11:33:41 -0400

    Add files via upload
    my Xcode

---
+++ KWK folder/storyBoardIntro/storyBoardIntro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  storyBoardIntro
+//
+//  Created by Lesley Valdovinos on 8/5/20.
+//  Copyright © 2020 Lesley Valdovinos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("This is a print statement - testing, testing")
+    }
+
+
+}
+
